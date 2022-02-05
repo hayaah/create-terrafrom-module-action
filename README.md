4b52e44dbb770b28a1f485e3f5cec0b773681a66
HaYa Abu Al Halawa <44801508+hayaah@users.noreply.github.com>
2022-02-05 22:21:00 +0200

    test

--- README.md
+++ README.md
@@ -1,21 +1,31 @@
 # Hello world javascript action
 
-This action prints "Hello World" or "Hello" + the name of a person to greet to the log.
+This action creates a terraform module within an organazation
 
 ## Inputs
 
-## `who-to-greet`
+## `module_name`
 
-**Required** The name of the person to greet. Default `"World"`.
+**Required** The name of the module to be created.
+
+## `token`
+
+**Required** The terrafomr account token
+
+## `provider`
+
+**Required**.
+
+## `registry_name`
+
+**Required**.
+
+## `organization`
+
+**Required**.
 
 ## Outputs
 
-## `time`
+## `response`
 
-The time we greeted you.
-
-## Example usage
-
-uses: actions/hello-world-javascript-action@v1.1
-with:
-  who-to-greet: 'Mona the Octocat'
+The response from the api call.
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,23 +1,17 @@
-const wait = require('../lib/wait');
-const process = require('process');
-const cp = require('child_process');
-const path = require('path');
+   
+    const createModule = require('../lib/index');
+const setInput = (name,value)=>
+    process.env[`INPUT_${name.replace(/ /g, '_').toUpperCase()}`]= value;
+ 
 
-test('throws invalid number', async () => {
-  await expect(wait('foo')).rejects.toThrow('milliseconds not a number');
-});
+    setInput("module_name", "module_name") 
+    setInput("token", "token") 
+    setInput("provider", "aws") 
+    setInput("registry_name", "private") 
+    setInput("organization", "infragod-test") 
 
-test('wait 500 ms', async () => {
-  const start = new Date();
-  await wait(500);
-  const end = new Date();
-  const delta = Math.abs(end - start);
-  expect(delta).toBeGreaterThanOrEqual(500);
-});
 
-// shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-  process.env.INPUT_MILLISECONDS = 500;
-  const ip = path.join(__dirname, '../lib/index.js');
-  console.log(cp.execSync(`node ${ip}`, { env: process.env }).toString());
+test('test calling create module function', async () => {
+
+ 
 });
